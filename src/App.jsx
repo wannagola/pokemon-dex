10fc17fbfada74fc500d8f69b88dc1c3acfbfa91
MINKI SUNG <162106492+wannagola@users.noreply.github.com>
2025-05-25 15:42:55 +0900

    feat: alert → react-toastify 토스트 알림으로 교체

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,14 @@
+// src/App.jsx
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { PokemonProvider } from "./context/PokemonContext";
 import Home from "./pages/Home";
 import Dex from "./pages/Dex";
 import PokemonDetail from "./pages/PokemonDetail";
+
+// ★ react-toastify
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function App() {
   return (
@@ -15,6 +20,15 @@ export default function App() {
           <Route path="/detail" element={<PokemonDetail />} />
         </Routes>
       </BrowserRouter>
+      {/* 토스트 컨테이너를 루트에 한 번만 배치 */}
+      <ToastContainer
+        position="top-right"
+        autoClose={2000}
+        hideProgressBar
+        newestOnTop
+        closeOnClick
+        pauseOnHover
+      />
     </PokemonProvider>
   );
 }
